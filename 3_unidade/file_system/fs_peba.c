7c16f7fbbaf62cf782e2766813fb661720e7dc03
Carlos Morais <carlos.morais.063@ufrn.edu.br>
2025-06-25 08:02:00 +0000

    Adicionando atividade de File System

---
+++ 3_unidade/file_system/fs_peba.c
@@ -0,0 +1,7 @@
+/* Criar um sistema de arquivos de somente leitura
+	-> Armazenamento em um sipositivo de blocos
+		* Tamanho do block pode ser fixo (e.g. 512 bytes)
+	-> Arquivos serão somente lidos
+	-> Busca por arquivos eficiente
+	Qual seria um formato adequado?
+*/
